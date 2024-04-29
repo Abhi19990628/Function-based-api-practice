fe44ca7c055e9ce17e2e56e4e4a09c819ab679da
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-04-30 02:40:05 +0530

    Update models.py

--- djangoapp2/models.py
+++ djangoapp2/models.py
@@ -4,4 +4,4 @@ class Abhishek(models.Model):
     name=models.CharField(max_length=10)
     age= models.IntegerField()
     sex=models.CharField(max_length=20)
-    color=models.CharField(max_length=20),
+    color=models.CharField(max_length=20)
