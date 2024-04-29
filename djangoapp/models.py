ca34604f1163a5c6d1334719f706b624247b06f5
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-04-29 16:47:40 +0530

    Update models.py

--- djangoapp/models.py
+++ djangoapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Rishab(models.Model):
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=50),
     age=models.IntegerField()
     sex=models.CharField(max_length=10)
     color=models.CharField(max_length=30)
